d361b35e4cf68212fbfee8d271184ebc6bb664b3
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 14:23:36 +0300

    first commit

---
+++ app/src/main/java/com/fingersoft/hc/black/CNST.kt
@@ -0,0 +1,17 @@
+package com.fingersoft.hc.black
+
+object CNST {
+
+    const val AF_DEV_KEY = "Kd6dVTAXvJubFrKP4CdgQW"
+    const val jsoupCheck = "9i2P"
+    const val ONESIGNAL_APP_ID = "3db1b521-78d3-4ac5-9f81-db896de4fb72"
+
+    var lru = "http://lunarchariot.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
